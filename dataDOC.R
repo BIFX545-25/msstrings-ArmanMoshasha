9fe3f4a6696ebdb695a6cb34c9347f39815b75ac
ArmanMoshasha <Armanmoshasha@gmail.com>
2025-02-13 20:07:33 -0500

    Week 4

---
+++ dataDOC.R
@@ -0,0 +1,8 @@
+#' Title
+#'
+#' some fragments with protein modifications that we might expect to find from CCR5 gene in a mass spec experiement
+#'
+#' @format A character vector of length 4 with peptide sequence
+#'
+#' @source data-raw/Data.R
+"MSfrags"
